65efcbeb7492d3e2d69fb7fe0b68b1704a153ba2
MuhammadUmar457 <umar.memon@hotmail.com>
2023-09-06 16:24:06 +1000

    appointment system for doctors

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\AppointmentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,8 +15,8 @@ use App\Http\Controllers\DoctorController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome_page', function () {
+    return view('welcome_page');
 });
 
 Route::post('/saveRoute', [UserController::class, 'save']) ->name('save');
@@ -23,5 +24,8 @@ Route::get('/doctor', [DoctorController::class, 'show'])->name('doctor');
 Route::post('/doctor/login', [DoctorController::class, 'login_doctor']) ->name('login_doctor');
 
 Route::get('/doctor_dashboard', function () {
-    return view('doctor_dashboard');
+    return view('welcome_page');
 });
+
+Route::post('/book-appointment', [AppointmentController::class, 'bookAppointment'])->name('book-appointment');
+Route::get('/search-doctor', [AppointmentController::class, 'searchDoctor']);
