2bfb409648589fdac15c883780c8bbe363d5c2d7
anika-cse <anika.cse19@gmail.com>
2023-09-29 12:31:03 +1000

    frontend files text updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,10 +15,8 @@
                                     <div class="banner-text">
                                         <h1 class="mb-0 oswald-bold fs-40 text-uppercase text-white">Your health is</h1>
                                         <h2 class="mb-0 oswald-bold fs-60 text-uppercase text-white">Our priority</h2>
-                                        <p class="mb-0 open-reg fs-13 text-white">Lorem Ipsum is simply dummy text of the
-                                            printing and
-                                            typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-                                            ever since the 1500s</p>
+                                        <p class="mb-0 open-reg fs-13 text-white">We ensure patient safety and quality of care as per international standard. 
+                                        We always try to adhere with legal compliance and give priority on employee health safety.</p>
                                     </div>
                                     <div class="banner-btn">
                                         <a href="#" class="cta-btn text-white oswald-med fs-16 text-uppercase">Make an
@@ -39,28 +37,24 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-8">
                         <div class="about-text text-center wow animate__animated animate__zoomIn animate__repeat-2">
-                            <h4 class="mb-0 text-uppercase oswald-reg fs-30">Welcom to Our Health Services</h4>
-                            <p class="mb-0 open-reg fs-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Voluptates consequatur
-                                ratione pariatur! Minus eligendi aperiam rem esse nam cumque hic atque non nostrum
-                                voluptatum repellendus
-                                facilis eum, quo, mollitia, quos corporis veritatis assumenda deserunt. Est, aut iste unde
-                                similique vel
-                                ratione animi labore minus rerum voluptatum vitae ut sequi, ex dolores earum, asperiores
-                                optio eius? Eligendi,
-                                illo. Cupiditate repellat sed ea beatae sit nesciunt. Nulla at iusto repellat non odit
-                                adipisci enim molestias
-                                aliquid suscipit cum debitis dolore ipsa placeat, nihil consequatur totam facilis ipsam
-                                voluptatibus praesentium
-                                provident laudantium similique sequi? Illo quae quis molestiae doloremque adipisci,
-                                cupiditate autem dolore.</p>
-                            <p class="mb-0 open-reg fs-16">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Provident harum vel sint dolor autem sequi non neque
-                                saepe quam est fugiat commodi eveniet mollitia amet ratione tempora dolore repellat ipsam,
-                                suscipit excepturi
-                                atque nemo incidunt. Inventore nisi sint repudiandae sequi possimus quidem! Consequuntur
-                                eaque doloribus
-                                architecto! Nobis sed dolorum ipsa!</p>
+                            <h4 class="mb-0 text-uppercase oswald-reg fs-30">Welcom to know us</h4>
+                            <p class="mb-0 open-reg fs-16">we are dedicated to a singular mission that providing exceptional 
+                                healthcare with compassion, expertise, and innovation. We are serving our community  with 
+                                standard for excellence in healthcare.
+
+                                Our mission is simple yet profound to enhance the well-being of our patients and community by 
+                                delivering comprehensive, patient-centered care. We are guided by a set of core values that include:</p>
+                            <p class="mb-0 open-reg fs-16">We treat each patient with empathy and respect, understanding 
+                                the unique challenges they face. 
+
+                                We are committed to achieving the highest standards in medical care, continuously 
+                                improving our services, and staying at the forefront of medical advancements.
+
+                                We actively engage with and support our community, promoting health and well-being beyond 
+                                our hospital walls.
+
+                                Patient safety is our top priority. We adhere to rigorous safety protocols and industry best 
+                                practices.</p>
                         </div>
                     </div>
                 </div>
