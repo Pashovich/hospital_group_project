0857dadb8a9627fbc2f57a8ecf41ece3fad9dbe2
raziasultana2638 <raziasultana2638@gmail.com>
2023-10-01 01:25:00 +1000

    update database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,11 +19,18 @@ return new class extends Migration
 
             $table->string('email');
             $table->string('phone')->nullable();
+            $table->date('dob')->nullable();
             $table->string('address')->nullable();
             $table->string('state')->nullable();
 
             $table->string('password');
             $table->string('role');
+            $table->integer('gender')->nullable();
+            $table->integer('marital_status')->nullable();
+            $table->integer('is_alcoholic')->nullable();
+            $table->integer('is_smoker')->nullable();
+            $table->integer('taking_medicine')->nullable();
+            $table->string('comments')->nullable();
             
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
